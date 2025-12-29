9e388acd08a8e819ca2fb749c55bd71818ae5d46
Jeewantha Dilshan <jeewantha.dgihan@gmail.com>
2025-12-30 00:34:11 +0530

    Update Board

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 board = [1,2,3,4,5,6,7,8,9]
 
 def display_board():
-    print(board[0],board[1],board[2])
-    print(board[3],board[4],board[5])
-    print(board[6],board[7],board[8])
+    print(f"{board[0]},{board[1]},{board[2]}")
+    print(f"{board[3]},{board[4]},{board[5]}")
+    print(f"{board[6]},{board[7]},{board[8]}")
+    
 
 
 # Ask Player 1 to choose X or O
