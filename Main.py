d80442ed61fc60ef806e0cb87b5159cc7b536131
Jeewantha Dilshan <jeewantha.dgihan@gmail.com>
2025-12-29 23:56:30 +0530

    Getting user Input

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
-def main():
- 
-    def get_User_Input():
-        User_One = "X"
-        User_Two = "O"
+boardboard = [1,2,3,4,5,6,7,8,9]
 
-        print("Select One: \"X\" or \"O\" ")
 
-    get_User_Input()
-    
-main()
+player1_symbol =input("Choose Symbol: \"X or O\" ").upper()
 
+while player1_symbol not in ["X", "O"]:
+    if player1_symbol == "X":
+        player2_symbol = "O"
+    else: player2_symbol = "X"
+
+
+print(player1_symbol)
+print(player2_symbol)
